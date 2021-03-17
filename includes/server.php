4aadedf040c8537a1c76d271db939f31878ccb28
Flamur Beqiraj <flamurbeqiraj95@gmail.com>
2021-03-17 12:51:36 +0100

    Check for database connection for configuration

--- includes/server.php
+++ includes/server.php
@@ -1,7 +1,13 @@
 <?php
-    if (!file_exists('dbcon.php')) {
-        echo json_encode(array('status' => 'no_db'));
-    } else {
-        echo json_encode(array('status' => 'db_found'));
+    // if (!file_exists('dbcon.php')) {
+    //     echo json_encode(array('status' => 'no_db'));
+    // } else {
+    //     echo json_encode(array('status' => 'db_found'));
+    // }
+    if (isset($_GET['case']) && $_GET['case'] == 'getDbConnStatus') {
+        $host = $_GET['hostname'];
+        $user = $_GET['username'];
+        $pass = $_GET['password'];
+        $thedb= $_GET['database'];
     }
 ?>
--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,31 @@ $('#checkConfiguration').click(function() {
 	var errorLog = '';
     var currentStep = 0;
 
-	
+    var host = $('#host').val();
+    var user = $('#user').val();
+    var pass = $('#pass').val();
+    var theDb= $('#thedb').val();
+
+	Swal.fire({
+        title: 'Configuring',
+        html: 'Status: <span id="statuslog">Trying database connection...</span><br><p class="text-muted fs-6">Please don\'t quit the process</p>',
+        showConfirmButton: false,
+        allowOutsisideClick: false,
+        allowEscapeKey: false
+    });
+
+    $.ajax({
+        type: 'get',
+        url: 'includes/server.php',
+        data: {
+            case:       'getDbConnStatus',
+            hostname:   host,
+            username:   user,
+            password:   pass,
+            database:   theDb
+        },
+        success:function() {
+
+        }
+    });
 });
